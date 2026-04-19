fd9147d280fab26694ab5d4530202943bc7cd625
yuto_kada <yuutokada0221@gmail.com>
2026-04-19 16:12:42 +0900

    Add files via upload

--- 2-3/src/Main7.java
+++ 2-3/src/Main7.java
@@ -1,10 +1,10 @@
 public class Main7 {
     public static void main(String[] args) {
         String firstname = "山田";
-        String lastname = " 太郎";
+        String lastname = "太郎";
         int age = 16;
         String email = "yamada@examle.com";
-        System.out.println("名前:" + firstname + lastname);
+        System.out.println("名前:" + firstname + " " + lastname);
         System.out.println("年齢:" + age +"歳");
         System.out.println("メールアドレス:" + email);
 
