5c5df407761b545572274580107491df9847ce46
yuto_kada <yuutokada0221@gmail.com>
2026-04-27 11:36:13 +0900

    Add files via upload

--- 2-12/src/Main3.java
+++ 2-12/src/Main3.java
@@ -2,6 +2,10 @@ public class Main3 {
     public static void main(String[] args) {
         int height = 5;
         for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= height - i; j++) {
+                System.out.print(" ");
+            }
+
             for (int j = 1; j <= i * 2 - 1; j++) {
                 System.out.print("*");
             }
