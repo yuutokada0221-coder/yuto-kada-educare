c202009866ffd1587001c1651c26dff6ebf4ae87
yuto_kada <yuutokada0221@gmail.com>
2026-06-25 23:43:26 +0900

    Add files via upload

--- 3-19/src/Library.java
+++ 3-19/src/Library.java
@@ -1,16 +1,18 @@
 public class Library {
-    static int availableBooks = 100;
+    private static int availableBooks = 100;
 
-    public void borrowBook() {
+    public static void borrowBook() {
         if (availableBooks <= 0) {
-            System.out.println("エラー: 貸出可能な本がありません");
+            System.out.println("エラー：貸出可能な本がありません");
         } else {
             availableBooks--;
+            System.out.println("1冊借りました");
         }
     }
 
-    public void returnBook() {
+    public static void returnBook() {
         availableBooks++;
+        System.out.println("本を返却しました");
     }
 
     public static int getAvailableBooks() {
--- 3-19/src/Main3.java
+++ 3-19/src/Main3.java
@@ -2,14 +2,10 @@ public class Main3 {
     public static void main(String[] args) {
         System.out.println("貸出可能な本: " + Library.getAvailableBooks() + "冊");
 
-        Library library = new Library();
-        
-        library.borrowBook();
-        System.out.println("1冊借りました");
+        Library.borrowBook();
         System.out.println("貸出可能な本: " + Library.getAvailableBooks() + "冊");
 
-        library.returnBook();
-        System.out.println("本を返却しました");
+        Library.returnBook();
         System.out.println("貸出可能な本: " + Library.getAvailableBooks() + "冊");
     }
 }
