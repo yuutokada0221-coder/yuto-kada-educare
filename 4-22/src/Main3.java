433a8f84e6c0c0543a2d5011765258489aa1bf08
yuto_kada <yuutokada0221@gmail.com>
2026-07-06 22:52:31 +0900

    Add files via upload

--- 4-22/src/Main3.java
+++ 4-22/src/Main3.java
@@ -1,33 +1,23 @@
-class Book {
-    private String title;
-    private String author;
-    private int price;
-
-    public Book(String title, String author, int price) {
-        this.title = title;
-        this.author = author;
-        this.price = price;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-}
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
 
 public class Main3 {
     public static void main(String[] args) {
-        Book book = new Book("Javaプログラミング", "山田 太郎", 2800);
-
-        System.out.println("タイトル: " + book.getTitle());
-        System.out.println("著者: " + book.getAuthor());
-        System.out.println("価格: " + book.getPrice());
+        try {
+            File file = new File("excersise.xml");
+            JAXBContext jaxbContext = JAXBContext.newInstance(Book.class);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            
+            Book book = (Book) unmarshaller.unmarshal(file);
+            
+            System.out.println("タイトル: " + book.getTitle());
+            System.out.println("著者: " + book.getAuthor());
+            System.out.println("価格: " + book.getPrice());
+            
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
     }
 }
