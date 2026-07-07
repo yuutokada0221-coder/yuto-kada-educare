442b1b3ffe91310bcb717f338717332a6e67a637
yuto_kada <yuutokada0221@gmail.com>
2026-07-07 10:38:47 +0900

    Add files via upload

--- 4-22/src/Main3.java
+++ 4-22/src/Main3.java
@@ -1,22 +1,28 @@
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
 import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
 
 public class Main3 {
     public static void main(String[] args) {
         try {
             File file = new File("excersise.xml");
-            JAXBContext jaxbContext = JAXBContext.newInstance(Book.class);
-            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-            
-            Book book = (Book) unmarshaller.unmarshal(file);
-            
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(file);
+
+            String title = doc.getElementsByTagName("title").item(0).getTextContent();
+            String author = doc.getElementsByTagName("author").item(0).getTextContent();
+            String priceStr = doc.getElementsByTagName("price").item(0).getTextContent();
+            int price = Integer.parseInt(priceStr); // 価格は計算できるように数値(int)に変換
+
+            Book book = new Book(title, author, price);
+
             System.out.println("タイトル: " + book.getTitle());
             System.out.println("著者: " + book.getAuthor());
             System.out.println("価格: " + book.getPrice());
-            
-        } catch (JAXBException e) {
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
